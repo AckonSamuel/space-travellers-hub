48bfe309c7ebc9e51e7c06acdda1e8127760402e
Tushar <singhtushar348@gmail.com>
2022-08-22 16:17:38 +0100

    Add Navlink to header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import logo from './planet.png';
 import './Header.css';
 
@@ -8,7 +9,12 @@ const Header = () => (
       <img className="logo" src={logo} alt="logo" />
       <h1>Space Traveler&apos;s Hub</h1>
     </div>
-    <nav />
+    <nav>
+      <li key={1}><NavLink to="/">Rockets</NavLink></li>
+      <li key={2}><NavLink to="/dragons">Dragons</NavLink></li>
+      <li key={3}><NavLink to="/Mission">Mission</NavLink></li>
+      <li key={4}><NavLink to="/My-Profile">My Profile</NavLink></li>
+    </nav>
   </header>
 );
 
