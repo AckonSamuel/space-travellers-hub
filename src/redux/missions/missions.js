8a67c8f528f4e50fd6a5f6b209c36992dab390e9
Ackon Samuel <ackons433@gmail.com>
2022-08-23 08:58:15 +0000

    Add action types

--- src/redux/missions/missions.js
+++ src/redux/missions/missions.js
@@ -1,2 +1,7 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
+
+const LOAD_MISSIONS = 'spacetravellers/missions/missions';
+const JOIN_MISSIONS = 'spacetravellers/missions/joinMissions';
+const LEAVE_MISSIONS = 'spacetravellers/missions/leaveMissions';
+const POPULATE_MISSIONS_PROFILE = 'spacetravellers/mission/populateMissionsProfile';
