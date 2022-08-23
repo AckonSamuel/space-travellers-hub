7c348cf6cce887c819123f16676a134454c06f9d
Tushar <singhtushar348@gmail.com>
2022-08-23 09:57:05 +0100

    stylelint fix

--- src/Components/dragons/dragons.js
+++ src/Components/dragons/dragons.js
@@ -1,11 +1,9 @@
 import React from 'react';
 
-const Dragon = ()=>{
-    return (
-        <section>
-            <h1>Dragons will appear here</h1>
-        </section>
-    )
-};
+const Dragon = () => (
+  <section>
+    <h1>Dragons will appear here</h1>
+  </section>
+);
 
-export default Dragon
+export default Dragon;
