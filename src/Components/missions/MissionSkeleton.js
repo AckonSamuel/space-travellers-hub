6f46c4127f0231cec2d7a12e0b59e067665b9caf
Ackon Samuel <ackons433@gmail.com>
2022-08-23 08:01:06 +0000

    Create mission component skeleton

---
+++ src/Components/missions/MissionSkeleton.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const MissionSkeleton = () => <div className="mission-table">
+    <h2>Hello</h2>
+    <p className="mission-description">
+        description
+    </p>
+    <p className="mission-status member">
+        Active member
+    </p>
+    <button type="button" className="mission-button leave"> Leave mission</button>
+</div>
+
+export default MissionSkeleton;
