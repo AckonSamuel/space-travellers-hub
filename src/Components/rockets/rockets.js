7c83132ec19f4359ed76cbbff8d978602595cdca
Tushar <singhtushar348@gmail.com>
2022-08-23 09:54:57 +0100

    write components to display list of rockets

--- src/Components/rockets/rockets.js
+++ src/Components/rockets/rockets.js
@@ -1,31 +1,32 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { fetchRockets, rocketReducer } from './actions_reducers';
+import { fetchRockets } from './actions_reducers';
+import './rockets.css';
 
 const Rockets = () => {
   const rockets = useSelector((state) => state.rocket);
   const dispatch = useDispatch();
 
-  useEffect(()=>{
-    dispatch(fetchRockets)
-  }, [])
-console.log(rockets)
+  useEffect(() => {
+    dispatch(fetchRockets);
+  }, []);
+
   return (
-  <section className='rockets'>
-    <ul>
-      {rockets.map((rocket) => (
-        <li>
-        <img src={rocket.flickr_images[0]} alt=''/>
-        <div>
-          <h3>{rocket.rocket_name}</h3>
-          <p>{rocket.description}</p>
-          <button type='button'>Reservation</button>
-        </div>
-      </li>
-      ))}
-    </ul>
-  </section>
-  )
+    <section className="rockets">
+      <ul className="rockets-list">
+        {rockets.map((rocket) => (
+          <li key={rocket.id} className="rocket-data">
+            <img src={rocket.flickr_images[0]} alt="rocket from space X" className="rocket-image" />
+            <div className="rocket-details">
+              <h3>{rocket.rocket_name}</h3>
+              <p>{rocket.description}</p>
+              <button type="button" className="rocket-btn">Reservation</button>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
 };
 
 export default Rockets;
