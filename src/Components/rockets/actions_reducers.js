033a4e216cae057a4be533cc05b5283b7a03a31e
Tushar <singhtushar348@gmail.com>
2022-08-23 09:56:51 +0100

    Add actions and reducers to fetch and store data from API

--- src/Components/rockets/actions_reducers.js
+++ src/Components/rockets/actions_reducers.js
@@ -1,25 +1,20 @@
 const FETCH_ROCKETS = 'FETCH_ROCKETS';
 const rocketApi = 'https://api.spacexdata.com/v3/rockets';
 
-const fetchRockets = async(dispatch) => {
-    try {
-        const response = await fetch(rocketApi).then(res => res.json())
-        dispatch({type: FETCH_ROCKETS, payload: response});
-        return response;
-    }
-    catch (error) {
-        console.log(error);
-    }
+const fetchRockets = async (dispatch) => {
+  try {
+    const response = await fetch(rocketApi).then((res) => res.json());
+    dispatch({ type: FETCH_ROCKETS, payload: response });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-
-const rocketReducer = (state=[], action) => {
-    if(action.type === FETCH_ROCKETS){
-        console.log(action.payload.forEach(a => console.log(a.description)));
-        return action.payload
-    }
-    else return state;
+const rocketReducer = (state = [], action) => {
+  if (action.type === FETCH_ROCKETS) {
+    return action.payload;
+  }
+  return state;
 };
 
-
-export {fetchRockets, rocketReducer};
+export { fetchRockets, rocketReducer };
