f39d51ed7fb0091da3cb95c92a3a879f2c6fa2ea
Ackon Samuel <ackons433@gmail.com>
2022-08-23 09:00:47 +0000

    Add mission reducer

--- src/redux/missions/missions.js
+++ src/redux/missions/missions.js
@@ -5,3 +5,25 @@ const LOAD_MISSIONS = 'spacetravellers/missions/missions';
 const JOIN_MISSIONS = 'spacetravellers/missions/joinMissions';
 const LEAVE_MISSIONS = 'spacetravellers/missions/leaveMissions';
 const POPULATE_MISSIONS_PROFILE = 'spacetravellers/mission/populateMissionsProfile';
+
+const initialState = [];
+
+const missionReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'spacetravellers/missions/missions/fulfilled':
+            return [...action.payload];
+        case 'spacetravellers/missions/joinMissions/fulfilled':
+            return [
+                ...state.map((mission) => (mission.id !== action.payload ? 
+                mission : { ...mission, reserved: true}))];
+        case 'spacetravellers/missions/leaveMissions/fulfilled':
+            return [
+                ...state.map((mission) => (mission.id !== action.payload ? 
+                    mission : { ...mission, reserved: false }))
+            ]
+        case 'spacetravellers/mission/populateMissionsProfile':
+            return [...state]
+        default: 
+        return state;
+    }
+};
