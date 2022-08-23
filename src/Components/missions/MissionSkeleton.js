fadde736db0a3ff84c6232297811c02d577bf630
Ackon Samuel <ackons433@gmail.com>
2022-08-23 08:07:39 +0000

    Create Missin table heads

--- src/Components/missions/MissionSkeleton.js
+++ src/Components/missions/MissionSkeleton.js
@@ -1,14 +1,16 @@
 import React from 'react';
 
-const MissionSkeleton = () => <div className="mission-table">
+const MissionSkeleton = () => (
+  <div className="mission-table">
     <h2>Hello</h2>
     <p className="mission-description">
-        description
+      description
     </p>
     <p className="mission-status member">
-        Active member
+      Active member
     </p>
     <button type="button" className="mission-button leave"> Leave mission</button>
-</div>
+  </div>
+);
 
 export default MissionSkeleton;
--- src/Components/missions/mission.js
+++ src/Components/missions/mission.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import MissionSkeleton from './MissionSkeleton';
 
 const Mission = () => (
-  <section>
-    <h1>Missions will appear here</h1>
+  <section className="super-mission">
+    <div className="header-container">
+      <p>Mission</p>
+      <p className="hola">Description</p>
+      <p className="status">Status</p>
+      <p className="join">Join</p>
+    </div>
+    <div className="mission-container>">
+      <MissionSkeleton />
+    </div>
   </section>
 );
 
