3103dcc3df0617ba5115f1b55d850d8580f1d837
Ackon Samuel <ackons433@gmail.com>
2022-08-23 19:53:55 +0000

    Fix linter errors

--- src/Components/missions/MissionSkeleton.js
+++ src/Components/missions/MissionSkeleton.js
@@ -1,41 +1,46 @@
 import React from 'react';
 import Proptypes from 'prop-types';
 import { useDispatch } from 'react-redux';
-import { JoinMission, LeaveMission } from '../../redux/missions'
+import { JoinMission, LeaveMission } from '../../redux/missions';
 import './missions.css';
 
-const MissionSkeleton = ({ name, description, id, status }) => {
+const MissionSkeleton = ({
+  name, description, id, status,
+}) => {
   const dispatch = useDispatch();
 
-  const missionParticipation = () => status === false ? dispatch(JoinMission(id)) : dispatch(LeaveMission(id));
-  
+  const missionParticipation = () => (status === false ? dispatch(JoinMission(id)) : dispatch(LeaveMission(id)));
+
   return (
     <>
-   { status === false ? (
-    <div className="mission-table" id={id}>
-    <h2>{name}</h2>
-    <p className="mission-description">
-      {description}
-    </p>
-    <div className="border">
-    <p className="mission-status">
-      Not a member
-    </p>
-    </div>
-    <button type="button" id={id} onClick={missionParticipation()} className="mission-button leave"> Join mission</button>
-  </div>) : (
-    <div id={id} className="mission-table">
-    <h2>{name}</h2>
-    <p className="mission-description">
-      {description}
-    </p>
-    <p className="mission-status member">
-      Active member
-    </p>
-    <button type="button" id={id} onClick={missionParticipation()} className="mission-button leave"> Leave mission</button>
-  </div>)}
+      { status === false ? (
+        <div className="mission-table" id={id}>
+          <h2>{name}</h2>
+          <p className="mission-description">
+            {description}
+          </p>
+          <div className="border">
+            <p className="mission-status">
+              Not a member
+            </p>
+          </div>
+          <button type="button" id={id} onClick={missionParticipation()} className="mission-button leave"> Join mission</button>
+        </div>
+      ) : (
+        <div id={id} className="mission-table">
+          <h2>{name}</h2>
+          <p className="mission-description">
+            {description}
+          </p>
+          <p className="mission-status member">
+            Active member
+          </p>
+          <button type="button" id={id} onClick={missionParticipation()} className="mission-button leave"> Leave mission</button>
+        </div>
+      )}
     </>
-)};
+  );
+};
 
 MissionSkeleton.propTypes = {
   name: Proptypes.string.isRequired,
