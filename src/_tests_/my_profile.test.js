ae06adc1134cf59eb25997d2dc1b6f25cdad5619
Ackon Samuel <ackons433@gmail.com>
2022-08-24 10:07:04 +0000

    Add test for my_profile component

--- src/_tests_/my_profile.test.js
+++ src/_tests_/my_profile.test.js
@@ -3,3 +3,12 @@ import { Provider } from 'redux-redux';
 import renderer from 'react-test-renderer';
 import store from '../redux/configureStore';
 import Myprofile from '../Components/my_profile/my_profile';
+
+it('Test if my_profile renderers correctly', () => {
+    const tree = renderer.create(
+        <Provider store={store}>
+            <Myprofile />
+        </Provider>,
+    );
+    expect(tree).toMatchSnapshot();
+})
