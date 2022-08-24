46d9ee72b356988ecc377085048adfc099fe8b35
Ackon Samuel <ackons433@gmail.com>
2022-08-24 09:52:00 +0000

    Test if mission renders

--- src/_tests_/missions.test.js
+++ src/_tests_/missions.test.js
@@ -3,3 +3,26 @@ import { Provider } from 'react-redux';
 import renderer from 'react-test-renderer';
 import store from '../redux/configureStore';
 import Mission from '../Components/missions/mission';
+
+describe('A test for missions component', () => {
+    it('Check whether mission component renders', () => {
+        const mission = {
+            mission_id: 1,
+            mission_name: 'Thaicom',
+            description: 'Thaicom is the name of a series of  communications satellites operated from Thailand, and also the name of Thaicom Public Company Limited, which is the company that owns and operates the Thaicom satellite fleet and other telecommunication businesses in Thailand and throughout the Asia-Pacific region. The satellite projects were named Thaicom by the King of Thailand, His Majesty the King Bhumibol Adulyadej, as a symbol of the linkage between Thailand and modern communications technology.',
+            reserved: false,
+        }
+        const missionTree = renderer.create(
+            <Provider store={store}>
+                <Mission
+                key={mission.mission_id}
+                description={mission.description}
+                name={mission.mission_name}
+                reserved={mission.reserved} 
+                />
+            </Provider>
+        );
+
+        expect(missionTree).toMatchSnapshot();
+    });
+});
