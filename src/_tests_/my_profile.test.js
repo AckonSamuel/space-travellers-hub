62c26ef0d3aeba4d8250a6ec513f323483cd8838
Ackon Samuel <ackons433@gmail.com>
2022-08-24 10:08:06 +0000

    Hot fix

--- src/_tests_/my_profile.test.js
+++ src/_tests_/my_profile.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Provider } from 'redux-redux';
+import { Provider } from 'react-redux';
 import renderer from 'react-test-renderer';
 import store from '../redux/configureStore';
 import Myprofile from '../Components/my_profile/my_profile';
