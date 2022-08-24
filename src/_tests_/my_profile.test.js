b0aca1650bff8a289a35a7d1e73d26f1d056166e
Ackon Samuel <ackons433@gmail.com>
2022-08-24 10:01:46 +0000

    Add necessary imports

--- src/_tests_/my_profile.test.js
+++ src/_tests_/my_profile.test.js
@@ -1,1 +1,5 @@
 import React from 'react';
+import { Provider } from 'redux-redux';
+import renderer from 'react-test-renderer';
+import store from '../redux/configureStore';
+import Myprofile from '../Components/my_profile/my_profile';
