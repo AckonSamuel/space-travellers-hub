4776706ab4dee2e2260c364909ac813343deeae2
Ackon Samuel <ackons433@gmail.com>
2022-08-24 09:40:30 +0000

    Add necessary imports

---
+++ src/_tests_/missions.test.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import renderer from 'react-test-renderer';
+import store from '../redux/configureStore';
+import Mission from '../Components/missions/mission';
